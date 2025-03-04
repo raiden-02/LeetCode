57d3ae5d807289fcd86955bc2635dfaa333e2be1
Mayur Reddy <54232956+raiden-02@users.noreply.github.com>
2025-03-04 17:43:45 -0600

    Update 1780. Check if Number is a Sum of Powers of Three.ts

--- 1780. Check if Number is a Sum of Powers of Three.ts
+++ 1780. Check if Number is a Sum of Powers of Three.ts
@@ -22,3 +22,31 @@ function checkPowersOfThree(n: number): boolean {
 
     return false;
 };
+
+
+
+// improved
+function checkPowersOfThree(n: number): boolean {
+    let rem: number = n;
+    let prev_cnt: number = -1;
+
+    while(rem > 0) {
+        let cur: number = 1;
+        let cnt: number = 0;
+
+        while(cur <= rem) {
+            cur *= 3;
+            cnt += 1;
+        }
+
+        if(cnt == prev_cnt) return false;
+        prev_cnt = cnt;
+
+        cur /= 3;
+
+        if(rem == cur) return true;
+        rem -= cur;
+    }
+
+    return false;
+};
